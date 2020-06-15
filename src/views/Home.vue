f96ddbd160f39fdeee01f5aa082e0452e0f6320f
Agustin De Luca <agusdeluca96@gmail.com>
2020-06-15 13:50:38 -0300

    Added Header section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="header">
+      <div class="header__logo-container">
+        <img src="@/assets/images/logo.png" alt="Logo" class="header__logo" />
+      </div>
+      <div class="header__text-container">
+        <h1 class="heading-primary">
+          <span class="heading-primary--main">Outdoors</span>
+          <span class="heading-primary--sub">is where life happens</span>
+        </h1>
+
+        <a href="#tours" class="btn btn--white btn--rounded btn--animated">Discover our tours</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
