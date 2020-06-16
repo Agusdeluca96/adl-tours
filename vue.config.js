4b18a2f1f44546bb4c9e3d07647623173063f2a8
Agustin De Luca <agusdeluca96@gmail.com>
2020-06-16 18:50:28 -0300

    Modified some PWA options

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   outputDir: "dist",
-  assetsDir: "assets"
+  assetsDir: "assets",
+
+  // PWA options
+  pwa: {
+    name: 'ADL Tours',
+    themeColor: '#69d457',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'assets/images/icons/favicon-32x32.png',
+      favicon16: 'assets/images/icons/favicon-16x16.png',
+      appleTouchIcon: 'assets/images/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'assets/images/icons/safari-pinned-tab.svg',
+      msTileImage: 'assets/images/icons/msapplication-icon-144x144.png'
+    }
+  }
 };
